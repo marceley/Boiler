cfc365a453d38e3eb58eb618e4597b4181069397
Marc Eley <marc@eley.dk>
2025-10-18 19:23:52 +0200

    favicon

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,18 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    href: "/favicon-light.svg",
+    type: "image/svg+xml",
+    media: "(prefers-color-scheme: light)",
+  },
+  {
+    rel: "icon",
+    href: "/favicon-dark.svg",
+    type: "image/svg+xml",
+    media: "(prefers-color-scheme: dark)",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import type { Route } from "./+types/home";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Boiler" },
-    { name: "description", content: "React Router Boilerplate" },
+    { name: "description", content: "Boiler gallery." },
   ];
 }
 
